2f6837e6da209604b2c228a05f3cfb5c66a6e29d
Umair Farooq <umair.farooq@dmechs.com>
2023-02-17 16:40:22 +0500

    added home button over classes on left navigation.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -11,30 +11,38 @@ import {
   AddCourseButton,
   AddCourseButtonContainer,
 } from "./AddCourseButton/styled";
-import { sortClasses } from "../../utils";
+import HouseSidingIcon from "@mui/icons-material/HouseSiding";
+import { Link as RouterLink } from "react-router-dom";
 
-const Sidebar: FC<SidebarProps> = ({ classes, handleOpenModal }) => (
-  <SidebarContainer>
-    <UserInfo user={TEST_USER} />
-    <Divider className="w-75 mb-1" color="#DE4CE1" />
-    <h6 className="text-center fw-bold p-0 mt-4 mb-2">Mis cursos</h6>
-    <LinkList>
-      {classes?.map?.((teacherClass: ClassInterface, index) => (
-        <SidebarLink
-          key={`side-bar-${teacherClass.id}-${index}`}
-          linkId={teacherClass.id}
-          linkTitle={teacherClass.alias}
-          linkRoute={`cursos/${teacherClass.id}/tablero`}
-        />
-      ))}
-    </LinkList>
-    <AddCourseButtonContainer onClick={() => handleOpenModal()}>
-      <AddCourseButton color="primary">
-        <AddIcon />
-      </AddCourseButton>
-    </AddCourseButtonContainer>
-    {/* <SignOutButton variant="text" color="primary" fullWidth>Cerrar sesión</SignOutButton> */}
-  </SidebarContainer>
-);
+const Sidebar: FC<SidebarProps> = ({ classes, handleOpenModal }) => {
+  return (
+    <SidebarContainer>
+      <UserInfo user={TEST_USER} />
+      <Divider className="w-75 mb-1" color="#DE4CE1" />
+      <h6 className="text-center fw-bold p-0 mt-4 mb-2">Mis cursos</h6>
+      <AddCourseButtonContainer as={RouterLink} to={"/"}>
+        <AddCourseButton color="primary">
+          <HouseSidingIcon />
+        </AddCourseButton>
+      </AddCourseButtonContainer>
+      <LinkList>
+        {classes?.map?.((teacherClass: ClassInterface, index) => (
+          <SidebarLink
+            key={`side-bar-${teacherClass.id}-${index}`}
+            linkId={teacherClass.id}
+            linkTitle={teacherClass.alias}
+            linkRoute={`cursos/${teacherClass.id}/tablero`}
+          />
+        ))}
+      </LinkList>
+      <AddCourseButtonContainer onClick={() => handleOpenModal()}>
+        <AddCourseButton color="primary">
+          <AddIcon />
+        </AddCourseButton>
+      </AddCourseButtonContainer>
+      {/* <SignOutButton variant="text" color="primary" fullWidth>Cerrar sesión</SignOutButton> */}
+    </SidebarContainer>
+  );
+};
 
 export default Sidebar;
