f90c1077396057a806ddb8411676fccd98b85ac0
Umair Farooq <umair.farooq@dmechs.com>
2023-02-21 19:08:19 +0500

    implemented adventures selected flow.

--- src/routes/Class/Adventures/interfaces.ts
+++ src/routes/Class/Adventures/interfaces.ts
@@ -1,8 +1,12 @@
+import { IAdventure } from "../../../global/interfaces";
+
 export interface GoalType {
   id: number | string;
   title: string;
   description?: string;
   image_url?: string;
+
+  adventures?: IAdventure[];
 }
 
 export interface GoalResponseType {
