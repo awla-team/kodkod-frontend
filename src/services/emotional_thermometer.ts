026be251e5be791ba11b6f7b5c3a7a410851e5d6
Umair Farooq <umair.farooq@dmechs.com>
2023-02-22 13:07:42 +0500

    implemented emotional_thermometer update and save functionality.

--- src/services/emotional_thermometer.ts
+++ src/services/emotional_thermometer.ts
@@ -1,6 +1,20 @@
 import http from "global/api";
 import Moment from "moment";
 
+interface SaveBody {
+  score: number;
+
+  challenge: string;
+
+  most_remarkable: string;
+
+  id_class: number | string;
+
+  date: Date;
+}
+
+interface UpdateBody extends Partial<SaveBody> {}
+
 export const getEmotionalThermometerByClassId = (
   classId: number | string,
   dateFrom?: Date,
@@ -27,4 +41,15 @@ export const getEmotionalThermometerByClassId = (
     }
   }
   return http.get(`/emotional-thermometer-by-classId/${classId}` + queryParams);
+};
+
+export const saveEmotionalThermometerDetails = (body: SaveBody) => {
+  return http.post("emotional-thermometer", body);
+};
+
+export const updateEmotionalThermometerDetails = (
+  id: number | string,
+  body: UpdateBody
+) => {
+  return http.put("emotional-thermometer/" + id, body);
 };
