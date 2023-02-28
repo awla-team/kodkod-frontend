fd9e387a832fe4a9571b5341882fba987942eb8d
Umair Farooq <umair.farooq@dmechs.com>
2023-02-28 18:01:02 +0500

    showing adventures details in adventure dialog.

--- src/global/interfaces.ts
+++ src/global/interfaces.ts
@@ -2,15 +2,6 @@ import "styled-components";
 import { Theme } from "@mui/material/styles";
 import Missions from "../routes/Class/Adventures/Adventure/Missions";
 
-declare module '@mui/material/styles/createPalette' {
-  interface Palette {
-      highlight: Palette['primary'];
-  }
-  interface PaletteOptions {
-      highlight: PaletteOptions['primary'];
-  }
-}
-
 interface CustomTheme {
   bg?: {
     main: string;
