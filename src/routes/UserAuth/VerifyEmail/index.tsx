96545b5884d1368b1f43fab988970748c2bb5a8e
Alvaro Arratia Ramirez <alvaroarratia.r@gmail.com>
2023-04-01 19:50:56 -0300

    added account creation confirm view

--- src/routes/UserAuth/VerifyEmail/index.tsx
+++ src/routes/UserAuth/VerifyEmail/index.tsx
@@ -1,31 +1,13 @@
-import {
-  Box,
-  Button,
-  CardContent,
-  CircularProgress,
-  Typography,
-} from "@mui/material";
-import { FetchStatus } from "global/enums";
-import React, { PropsWithChildren, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
-import {
-  ForgotPasswordCard,
-  ForgotPasswordContainer,
-} from "../ForgotPassword/styled";
 import { Link as RouterLink } from "react-router-dom";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import Toaster from "utils/Toster";
 import { resendEmailVerification, verifyEmail } from "services/auth";
-
-const CustomCard: React.FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <ForgotPasswordContainer className="d-flex flex-column">
-      <ForgotPasswordCard variant="outlined">
-        <CardContent className="p-5">{children}</CardContent>
-      </ForgotPasswordCard>
-    </ForgotPasswordContainer>
-  );
-};
+import AuthCard from "components/AuthCard";
+import { FetchStatus } from "global/enums";
+import { ForgotPasswordContainer } from "../ForgotPassword/styled";
 
 const VerifyEmail: React.FC = () => {
   const { token, userId } = useParams();
@@ -72,7 +54,57 @@ const VerifyEmail: React.FC = () => {
 
   if (emailVerified === FetchStatus.Error)
     return (
-      <CustomCard>
+      <ForgotPasswordContainer className="d-flex flex-column">
+        <AuthCard>
+          <Button
+            className="mb-2"
+            startIcon={<ArrowBackIosIcon />}
+            component={RouterLink}
+            to="/signin"
+          >
+            Volver al inicio de sesión
+          </Button>
+          <Typography component="h4" variant="h5" className="mb-1">
+            Ha ocurrido un error
+          </Typography>
+          <Typography component="span" variant="body2" color="gray">
+            El link de verificación ha expirado. Puedes hacer clic en el botón "
+            <b>Reenviar link de verificación</b>" para enviar un nuevo link a tu
+            email.
+          </Typography>
+          <Box
+            className="action__container"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            gap={1}
+            mt={2}
+          >
+            <Button
+              disabled={
+                emailResend === FetchStatus.Pending ||
+                emailResend === FetchStatus.Success
+              }
+              fullWidth
+              size="large"
+              className="submit__button"
+              variant="contained"
+              onClick={sendEmailVerification}
+            >
+              {emailResend === FetchStatus.Pending ? (
+                <CircularProgress />
+              ) : (
+                "Reenviar link de verificación"
+              )}
+            </Button>
+          </Box>
+        </AuthCard>
+      </ForgotPasswordContainer>
+    );
+
+  return (
+    <ForgotPasswordContainer className="d-flex flex-column">
+      <AuthCard>
         <Button
           className="mb-2"
           startIcon={<ArrowBackIosIcon />}
@@ -82,59 +114,13 @@ const VerifyEmail: React.FC = () => {
           Volver al inicio de sesión
         </Button>
         <Typography component="h4" variant="h5" className="mb-1">
-          Ha ocurrido un error
+          Tu email ha sido verificado con exito
         </Typography>
         <Typography component="span" variant="body2" color="gray">
-          El link de verificación ha expirado. Puedes hacer clic en el botón "
-          <b>Reenviar link de verificación</b>" para enviar un nuevo link a tu
-          email.
+          Ahora podrás usar kodkod sin problemas
         </Typography>
-        <Box
-          className="action__container"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          gap={1}
-          mt={2}
-        >
-          <Button
-            disabled={
-              emailResend === FetchStatus.Pending ||
-              emailResend === FetchStatus.Success
-            }
-            fullWidth
-            size="large"
-            className="submit__button"
-            variant="contained"
-            onClick={sendEmailVerification}
-          >
-            {emailResend === FetchStatus.Pending ? (
-              <CircularProgress />
-            ) : (
-              "Reenviar link de verificación"
-            )}
-          </Button>
-        </Box>
-      </CustomCard>
-    );
-
-  return (
-    <CustomCard>
-      <Button
-        className="mb-2"
-        startIcon={<ArrowBackIosIcon />}
-        component={RouterLink}
-        to="/signin"
-      >
-        Volver al inicio de sesión
-      </Button>
-      <Typography component="h4" variant="h5" className="mb-1">
-        Tu email ha sido verificado con exito
-      </Typography>
-      <Typography component="span" variant="body2" color="gray">
-        Ahora podrás usar kodkod sin problemas
-      </Typography>
-    </CustomCard>
+      </AuthCard>
+    </ForgotPasswordContainer>
   );
 };
 
