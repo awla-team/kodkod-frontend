114875d77c82f436bedd4bab5045c351d6aff47d
bastian diaz <basty.diaaz@gmail.com>
2023-04-12 23:24:12 -0400

    run prettier

--- src/components/RewardCard/styled.tsx
+++ src/components/RewardCard/styled.tsx
@@ -12,26 +12,25 @@ export const CustomCard = styled(Card)`
   border-radius: 8px;
   position: relative;
 
-  background: rgb(252,238,255);
-  background: linear-gradient(360deg, rgba(252,238,255,1) 0%, rgba(210,210,255,1) 100%);
-  border: 1px solid rgb(252,238,255);
+  background: rgb(252, 238, 255);
+  background: linear-gradient(360deg, rgba(252, 238, 255, 1) 0%, rgba(210, 210, 255, 1) 100%);
+  border: 1px solid rgb(252, 238, 255);
 
   .reward-img > img {
     width: 100px;
-    
   }
 
   .points-container {
     position: absolute;
     padding: 4px 16px;
     box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.1);
-    background: rgb(242,251,250);
-    background: linear-gradient(180deg, rgba(242,251,250,1) 0%, rgba(255,255,255,1) 100%);
+    background: rgb(242, 251, 250);
+    background: linear-gradient(180deg, rgba(242, 251, 250, 1) 0%, rgba(255, 255, 255, 1) 100%);
     bottom: 0;
     border: 1px solid #dadada;
     border-radius: 16px 16px 0px 0px;
     border-bottom: none;
-  
+
     img {
       width: 24px;
       height: 24px;
