c605973662d3d3ea8e6e211792a654f2ef441d25
Alvaro Arratia Ramirez <alvaro@kodkod.cl>
2023-08-24 01:20:57 -0400

    feat: created ContentBox component

---
+++ src/components/ContentBox/index.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material';
+import styled from 'styled-components';
+
+const ContentBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: fit-content;
+  background: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.08);
+  border-radius: 8px;
+`;
+
+export default ContentBox;
