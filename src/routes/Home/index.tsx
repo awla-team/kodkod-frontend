a14e32dccac00019096105fb7ca3039f44478c74
Bastian Diaz <bastian.diaz@cat.com>
2023-08-27 19:40:06 -0400

    Add react tour

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,21 +1,33 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import WelcomePage from './WelcomePage';
 import MyClasses from './MyClasses';
 import { useOutletContext } from 'react-router-dom';
 import { ModifiedIClass } from 'global/interfaces';
+import { useTour } from '@reactour/tour';
+import { HomeOnboarding } from 'utils/Onboardings/HomeOnboarding';
 
 const HomePage: FC = () => {
+  const { setIsOpen, setSteps } = useTour();
   const { classes, handleOpenModal, getClassesData } = useOutletContext() as {
     classes: Array<ModifiedIClass>;
     handleOpenModal: () => void;
     getClassesData: () => void;
   };
 
-  return !classes?.length ? (
-    <WelcomePage handleOpenModal={handleOpenModal} />
-  ) : (
-    <MyClasses classes={classes} getClassesData={getClassesData} />
-  );
+  useEffect(() => {
+    setSteps(HomeOnboarding);
+    setIsOpen(true);
+  }, []);
+
+  return (
+    <div className="d-flex w-100">
+      {!classes?.length ? (
+        <WelcomePage handleOpenModal={handleOpenModal} />
+      ) : (
+        <MyClasses classes={classes} getClassesData={getClassesData} />
+      )}
+    </div>
+  )
 };
 
 export default HomePage;
