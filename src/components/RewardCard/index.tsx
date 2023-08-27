5e97ea4ddd854c16abdb89cdd4c271845917e39e
Alvaro Arratia Ramirez <alvaro@kodkod.cl>
2023-08-27 10:19:15 -0400

    refactor: RewardCard styles & props

--- src/components/RewardCard/index.tsx
+++ src/components/RewardCard/index.tsx
@@ -1,21 +1,32 @@
-import React, { useState } from 'react';
-import { Typography, TextField, IconButton } from '@mui/material';
+import { useState } from 'react';
+import { Typography, TextField } from '@mui/material';
 import { IRewardCardProps } from './interfaces';
-import { CustomCard } from './styled';
-import kodcoinIcon from './../../assets/images/kodcoin.png';
+import {
+  EditRewardActionsContainer,
+  EditRewardButton,
+  RewardCardContainer,
+  RewardCardContent,
+  RewardCardHeader,
+  RewardDescriptionInput,
+  RewardImg,
+} from './styled';
 import CloseIcon from '@mui/icons-material/Close';
 import CheckIcon from '@mui/icons-material/Check';
 import EditIcon from '@mui/icons-material/Edit';
-import http from 'global/api';
+import RewardPoints from './RewardPoints';
+import { AxiosError, AxiosResponse } from 'axios';
+import UsedRewardCount from './UsedRewardCount';
 
-const RewardCard: React.FC<IRewardCardProps> = ({
+const RewardCard = ({
   edit,
   rewardId,
   title,
   icon,
   description,
   requiredPoints,
-}) => {
+  order,
+  usedCount,
+}: IRewardCardProps) => {
   const [editMode, setEditMode] = useState(false);
   const [newTitle, setNewTitle] = useState(title);
   const [newDescription, setNewDescription] = useState(description);
@@ -32,93 +43,120 @@ const RewardCard: React.FC<IRewardCardProps> = ({
 
   const editReward = () => {
     edit(rewardId, newTitle, newDescription)
-      .then((result: any) => {
-        if (result?.data?.responseData === 1) setEditMode(false);
+      .then((response: AxiosResponse) => {
+        if (response?.data?.responseData === 1) setEditMode(false);
       })
-      .catch((err: any) => {
-        console.error(err);
+      .catch((error: AxiosError) => {
+        console.error(error);
       });
   };
 
   return (
-    <CustomCard
-      variant="outlined"
-      className="d-flex flex-column py-4 px-3 mb-3 align-items-center position-relative"
-    >
-      {editMode ? (
-        <div
-          className="d-flex"
-          style={{
-            position: 'absolute',
-            top: '10px',
-            right: '10px',
-          }}
-        >
-          <IconButton color="primary" onClick={editReward}>
-            <CheckIcon />
-          </IconButton>
-          <IconButton color="error" onClick={cancelEditMode}>
-            <CloseIcon />
-          </IconButton>
-        </div>
-      ) : rewardId ? (
-        <IconButton
-          sx={{
-            position: 'absolute',
-            top: '10px',
-            right: '10px',
-          }}
-          color="inherit"
-          onClick={activateEditMode}
-        >
-          <EditIcon />
-        </IconButton>
-      ) : null}
-      <div className="d-flex reward-img flex-fill mb-3 align-items-center justfy-content-center">
-        <img src={icon} alt="" />
-      </div>
-      <div className="d-flex flex-column align-items-center justify-content-end pb-5">
-        <div className="d-flex flex-column align-items-center">
-          {editMode ? (
-            <TextField
-              className="mb-1"
-              size="small"
-              type="text"
-              value={newTitle}
-              placeholder={title}
-              onChange={(event) => {
-                setNewTitle(event.target.value);
-              }}
-            />
-          ) : (
-            <Typography className="mb-1" variant="body1" fontWeight="bold">
-              {title}
-            </Typography>
-          )}
-          {editMode ? (
-            <TextField
-              value={newDescription}
-              size="small"
-              type="text"
-              placeholder={description}
-              onChange={(event) => {
-                setNewDescription(event.target.value);
-              }}
-            />
-          ) : (
-            <Typography variant="body2" textAlign="center">
-              {description}
-            </Typography>
-          )}
-        </div>
-        <div className="d-flex points-container align-items-center gap-1 mt-2">
-          <img src={kodcoinIcon} alt="" />
-          <Typography variant="h6" fontWeight="bold">
-            {requiredPoints}
+    <RewardCardContainer variant="outlined">
+      <EditRewardActionsContainer>
+        {editMode && (
+          <>
+            <EditRewardButton
+              color="inherit"
+              variant="contained"
+              onClick={editReward}
+              startIcon={<CheckIcon />}
+            >
+              Guardar
+            </EditRewardButton>
+            <EditRewardButton
+              color="inherit"
+              variant="contained"
+              onClick={cancelEditMode}
+              startIcon={<CloseIcon />}
+            >
+              Cancelar
+            </EditRewardButton>
+          </>
+        )}
+        {!editMode && !!rewardId && (
+          <EditRewardButton
+            color="inherit"
+            variant="contained"
+            onClick={activateEditMode}
+            startIcon={<EditIcon />}
+          >
+            Editar
+          </EditRewardButton>
+        )}
+      </EditRewardActionsContainer>
+      <RewardCardHeader>
+        <UsedRewardCount count={usedCount} />
+        {!!order && (
+          <Typography
+            color="white"
+            variant="body1"
+            fontWeight="bold"
+            fontSize="32px"
+          >
+            {order}
           </Typography>
+        )}
+      </RewardCardHeader>
+      <RewardCardContent>
+        <RewardImg src={icon} alt="" />
+        <div className="d-flex flex-column align-items-center justify-content-end">
+          <div className="d-flex flex-column align-items-center">
+            {editMode ? (
+              <TextField
+                className="mb-1"
+                size="small"
+                type="text"
+                value={newTitle}
+                placeholder={title}
+                color="primary"
+                sx={{
+                  input: { padding: '4px' },
+                }}
+                onChange={(event) => {
+                  setNewTitle(event.target.value);
+                }}
+              />
+            ) : (
+              <Typography
+                className="mb-1"
+                variant="body1"
+                fontWeight="bold"
+                fontSize="20px"
+                sx={{
+                  marginBottom: '8px',
+                }}
+              >
+                {title}
+              </Typography>
+            )}
+            {editMode ? (
+              <RewardDescriptionInput
+                minRows={2}
+                maxRows={4}
+                value={newDescription}
+                placeholder={description}
+                onChange={(event) => {
+                  setNewDescription(event.target.value);
+                }}
+              />
+            ) : (
+              <Typography
+                variant="body2"
+                textAlign="center"
+                fontSize="16px"
+                sx={{
+                  padding: '8px',
+                }}
+              >
+                {description}
+              </Typography>
+            )}
+          </div>
+          <RewardPoints points={requiredPoints as number} />
         </div>
-      </div>
-    </CustomCard>
+      </RewardCardContent>
+    </RewardCardContainer>
   );
 };
 
