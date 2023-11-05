e5fc2d08e371be8b935e24d848afd9ab229b620b
Alvaro Arratia Ramirez <alvaro@kodkod.cl>
2023-11-05 12:52:33 -0300

    fix: add termometer chart types

--- src/components/EmotionalThermometer/utils.ts
+++ src/components/EmotionalThermometer/utils.ts
@@ -1,5 +1,9 @@
 import moment from 'moment';
 import * as Yup from 'yup';
+import {
+  type TermometerChartData,
+  type EmotionalTermometer,
+} from './interfaces';
 
 export const CATEGORY_MAPPING: Readonly<Record<number, string>> = {
   0: 'Muy malo',
@@ -17,14 +21,17 @@ export const validationSchema = () => {
   });
 };
 
-export const termometerRecordAdapter = (termometerRecords: any[]) => {
-  const data = termometerRecords.map((termometerRecord: any) => {
-    const { score, date, ...otherProps } = termometerRecord;
-    return {
-      date: moment.utc(date).local().valueOf(),
-      category: CATEGORY_MAPPING[score],
-      ...otherProps,
-    };
-  });
+export const termometerRecordAdapter = (
+  termometerRecords: EmotionalTermometer[]
+): TermometerChartData[] => {
+  const data = termometerRecords.map(
+    (termometerRecord: EmotionalTermometer) => {
+      const { score, date } = termometerRecord;
+      return {
+        date: moment.utc(date).local().valueOf(),
+        category: CATEGORY_MAPPING[score],
+      };
+    }
+  );
   return data;
 };
