49d5eed03af86ee573f1a7e820a809b07b539816
Nicolas Alejandro Pereira Vega <nicopv.dev@gmail.com>
2024-06-19 12:25:15 -0400

    fix: remove query-string dependency, change service name

--- src/services/units.ts
+++ src/services/units.ts
@@ -1,15 +1,11 @@
 import { type KodkodResponse } from 'api/types/custom-response';
 import http from 'global/api';
 import type Unit from 'types/models/Unit';
-import queryString from 'query-string';
 
 const ROOT_PATH = '/unit';
 
 const constructRoute = (path: string) => `${ROOT_PATH}${path}`;
 
 export const getUnitsBySubject = async (query: { subject_id: string }) => {
-  const parseQuery = queryString.stringify(query);
-  return await http.get<KodkodResponse<Unit[]>>(
-    constructRoute(`/search?${parseQuery}`)
-  );
+  return await http.get<KodkodResponse<Unit[]>>(constructRoute(`/search`));
 };
