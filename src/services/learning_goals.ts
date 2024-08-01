0ccc5d18a54207960433e06edfd47b668a2ef7ff
DanielTampe <d.tampe01@ufromail.cl>
2024-08-01 14:33:26 -0400

    bugfix: applied fix to learning goals visualization

--- src/services/learning_goals.ts
+++ src/services/learning_goals.ts
@@ -1,5 +1,4 @@
 import http from 'global/api';
-import type ILearningGoal from 'types/models/LearningGoal';
 
 export const getLearningGoals = async () => {
   return await http.get('learning-goal');
@@ -9,8 +8,6 @@ export const getLearningGoalByID = async (id: number | string) => {
   return await http.get('learning-goal/' + id);
 };
 
-export const getLearningGoalsByUnit = async (
-  unitId: number | string
-): Promise<ILearningGoal[]> => {
+export const getLearningGoalsByUnit = async (unitId: number | string) => {
   return await http.get('learning-goal/get-by-unit-id/' + unitId);
 };
