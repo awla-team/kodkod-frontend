a11afeab41905e3387327e3650c1178431ea5247
DanielTampe <d.tampe01@ufromail.cl>
2024-09-02 13:53:36 -0400

    -added LessonDetails view

--- src/services/activities.ts
+++ src/services/activities.ts
@@ -1,6 +1,10 @@
 import { type IActivitySaved } from './../types/models/Activity';
 import http from 'global/api';
 
+export const getActivityByLessonId = async (lessonId: number) => {
+  return await http.get(`activity?lesson_id=${lessonId}`);
+};
+
 export const saveActivity = async (activity: IActivitySaved) => {
   return await http.post('activity', activity);
 };
