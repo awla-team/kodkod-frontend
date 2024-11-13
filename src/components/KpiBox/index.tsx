2f60dd313851f03fda7d3d45633e4d4c029bbf3d
DanielTampe <d.tampe01@ufromail.cl>
2024-11-13 14:24:30 -0300

    KOD-120 feat: Kpibox added

---
+++ src/components/KpiBox/index.tsx
@@ -0,0 +1,38 @@
+import { Tooltip } from '@mui/material';
+import { type ReactNode, type FC } from 'react';
+import HelpIcon from '@mui/icons-material/Help';
+
+interface Props {
+  icon: ReactNode;
+  title: string;
+  value: string;
+  helperText: string | null;
+}
+
+const KpiBox: FC<Props> = ({ icon, helperText, title, value }) => {
+  return (
+    <div className='tw-flex tw-justify-between tw-w-full tw-h-[100px] tw-border-solid tw-rounded-lg tw-border-2 tw-border-zinc-300'>
+      <div className='tw-flex tw-p-[24px] tw-gap-[24px]'>
+        <div className='tw-flex tw-justify-center tw-items-center'>{icon}</div>
+        <div className='tw-flex tw-flex-col tw-w-full'>
+          <div>
+            <b>{value}</b>
+          </div>
+          <div>{title}</div>
+        </div>
+      </div>
+      <div className='tw-flex tw-m-2'>
+        {helperText != null && (
+          <Tooltip className='' title={helperText}>
+            <HelpIcon
+              fontSize='small'
+              className='tw-text-gray-500 tw-ml-3 tw-mt-1 hover:tw-cursor-pointer'
+            />
+          </Tooltip>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default KpiBox;
