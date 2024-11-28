b2e34b4df07d57acbb86d6a3f5b784eeacc94eef
Nicolas Alejandro Pereira Vega <103705840+nicopv-dev@users.noreply.github.com>
2024-11-28 18:56:41 -0300

    KOD-119 added motivation scale (#220)
    * added motivation scale
    
    * added bg gradient class
    
    * added icon
    
    * fix show all icons into motivation bar
    
    * refactor: add responsiveness to component
    
    * added MotivationScale component into Analysis page
    
    ---------
    
    Co-authored-by: Alvaro Arratia Ramirez <alvaro@kodkod.cl>

---
+++ src/components/MotivationScale/index.tsx
@@ -0,0 +1,102 @@
+import React, { useMemo } from 'react';
+import fire20 from 'assets/images/fire-20.png';
+import fire40 from 'assets/images/fire-40.png';
+import fire60 from 'assets/images/fire-60.png';
+import fire80 from 'assets/images/fire-80.png';
+import passion from 'assets/images/passion.png';
+import InfoIcon from '@mui/icons-material/Info';
+
+import { cn } from 'utils/methods';
+
+const SCALES = [
+  { min: 1, max: 20, label: 'Muy baja' },
+  { min: 21, max: 40, label: 'Baja' },
+  { min: 41, max: 60, label: 'Media' },
+  { min: 61, max: 80, label: 'Alta' },
+  { min: 81, max: 100, label: 'Muy alta' },
+];
+
+const ICONS = [
+  { icon: fire20, position: 16 },
+  { icon: fire40, position: 36 },
+  { icon: fire60, position: 56 },
+  { icon: fire80, position: 76 },
+];
+
+interface Props {
+  motivationLevel: number;
+}
+
+/**
+ * MotivationScale component displays the motivation level of a course.
+ * It shows a label indicating the motivation level and a visual representation
+ * of the motivation scale with icons and a progress bar.
+ *
+ * @component
+ * @param {number} props.motivationLevel - The current motivation level of the course.
+ *
+ * @returns {JSX.Element} The rendered MotivationScale component.
+ *
+ * @example
+ * <MotivationScale motivationLevel={75} />
+ */
+const MotivationScale: React.FC<Props> = ({ motivationLevel }) => {
+  const activeScale = useMemo(
+    () =>
+      SCALES.find(
+        ({ min, max }) => motivationLevel >= min && motivationLevel <= max
+      ),
+    [motivationLevel]
+  );
+
+  const hiddenPercentage = useMemo(() => {
+    return 100 - motivationLevel;
+  }, [motivationLevel]);
+
+  return (
+    <div className='tw-flex tw-justify-between tw-items-center tw-gap-2 tw-flex-wrap'>
+      <div className='tw-flex tw-items-center tw-gap-2'>
+        <img
+          alt='fire'
+          src={passion}
+          className='tw-h-16 tw-w-16 tw-object-contain'
+        />
+        <div className='tw-flex tw-flex-col'>
+          <div className='tw-flex tw-items-center tw-gap-2'>
+            <span>Motivación del curso</span>
+            <InfoIcon fontSize='small' className='tw-text-zinc-500' />
+          </div>
+          <h4 className='tw-font-bold tw-text-3xl tw-my-0'>
+            {activeScale?.label}
+          </h4>
+        </div>
+      </div>
+      <div className='tw-h-10 tw-bg-zinc-100 tw-rounded-full tw-py-2.5 tw-px-3 tw-w-full tw-relative tw-mt-4 md:tw-w-1/2 md:tw-mt-0'>
+        <div className='tw-relative tw-rounded-full tw-h-full tw-flex tw-items-center tw-motivation-full-gradient px-2'>
+          <div className='tw-h-[1px] tw-w-full tw-bg-white tw-z-20' />
+          {/* Iconos posicionados */}
+          {ICONS.map(({ icon, position }, index) => (
+            <img
+              key={index}
+              src={icon}
+              alt={`icon-${index}`}
+              className={cn(
+                'tw-h-10 tw-object-contain tw-absolute -tw-top-4 tw-z-20'
+              )}
+              style={{ left: `${position}%` }}
+            />
+          ))}
+          {/* Progreso oculto */}
+          <div className='tw-w-full tw-h-full tw-absolute tw-top-0 tw-left-0 tw-z-10 tw-flex tw-justify-end'>
+            <div
+              className='tw-h-full tw-bg-zinc-100'
+              style={{ width: `${hiddenPercentage}%` }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MotivationScale;
